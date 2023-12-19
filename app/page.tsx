88fd476f01daa95b323ea03c200d0651e566a6ff
aaronJau21 <aaronjau21@gmail.com>
2023-12-19 12:31:19 -0500

    Documentación

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,24 @@
-'use client'
+"use client";
 
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import MarkerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
-import MarkerShadow from "../node_modules/leaflet/dist/images/marker-shadow.png";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
-export default function Home() {
+export default function Home(props: any) {
+  const { position, zoom } = props;
+
   return (
-    <div>
-      <MapContainer
-        style={{
-          height: "100vh",
-          width: "100vw",
-        }}
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          icon={
-            new L.Icon({
-              iconUrl: MarkerIcon.src,
-              iconRetinaUrl: MarkerIcon.src,
-              iconSize: [25, 41],
-              iconAnchor: [12.5, 41],
-              popupAnchor: [0, -41],
-              shadowUrl: MarkerShadow.src,
-              shadowSize: [41, 41],
-            })
-          }
-          position={[51.505, -0.09]}
-        ></Marker>
-      </MapContainer>
-    </div>
+    <MapContainer center={position} zoom={zoom} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
